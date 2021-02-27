5d8456d2630857169babfbddc9dc33dba300f68b
indePhoenixx <calebwade47@gmail.com>
2021-02-27 00:50:30 -0600

    My first commit.

--- app/src/main/java/com/indephoenixx/flashcardapp/MainActivity.java
+++ app/src/main/java/com/indephoenixx/flashcardapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.indephoenixx.flashcardapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //simplifying "findViewById(R.id.nameOfView)"
+        TextView Answer =  findViewById(R.id.Flash_Answer);
+        TextView Question =  findViewById(R.id.Flash_Question);
+
+        //making question Invisible and Answer visible on touch
+        Question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Answer.setVisibility(View.VISIBLE);
+                Question.setVisibility(View.INVISIBLE);
+            }
+        });
+        Answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Answer.setVisibility(View.INVISIBLE);
+                Question.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
+
 }
